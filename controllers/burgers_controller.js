87040e1cc79e54068c65e00328869febb4e4353b
eternal golden bread <codylewisbrock@gmail.com>
2020-01-14 22:13:27 -0800

    adds file structure

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,18 @@
+// Need to create the routes...
+
+// Inside the burgers_controller.js file, import the following:
+// Express
+var express = require("express");
+// burger.js
+var burger = require("../models/burger.js");
+
+// Create the router for the app... 
+var router = express.Router();
+
+
+
+
+
+
+// ...and export the router at the end of your file.
+module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,17 @@
+// Need to create functions taht will interact with database...
+
+// Inside burger.js, import orm.js into burger.js
+var orm = require("../config/orm.js");
+
+
+//Also inside burger.js, create the code 
+// that will call the ORM functions 
+// using burger specific input for the ORM.
+
+var burger = {
+
+}
+
+
+// Export at the end of the burger.js file.
+module.exports = burger;
