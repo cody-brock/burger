49c7e1655990c379e00794cd5e65503fac3fd911
eternal golden bread <codylewisbrock@gmail.com>
2020-01-15 21:46:24 -0800

    adds code into the controller, orm, and model.  beginning work on burger.js script file, need to complete the views

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,4 +1,4 @@
-// Need to create the routes...
+// Might be good to go - CB 1-15
 
 // Inside the burgers_controller.js file, import the following:
 // Express
@@ -8,6 +8,63 @@ var burger = require("../models/burger.js");
 
 // Create the router for the app... 
 var router = express.Router();
+
+
+
+
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", function(req, res) {
+  burger.create([
+    "name"
+  ], [
+    req.body.name
+  ], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+// router.delete("/api/cats/:id", function(req, res) {
+//   var condition = "id = " + req.params.id;
+
+//   cat.delete(condition, function(result) {
+//     if (result.affectedRows == 0) {
+//       // If no rows were changed, then the ID must not exist, so 404
+//       return res.status(404).end();
+//     } else {
+//       res.status(200).end();
+//     }
+//   });
+// });
+
 
 
 
--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,4 @@
-// Need to create functions taht will interact with database...
+// Should be good to go (CB 1-15)
 
 // Inside burger.js, import orm.js into burger.js
 var orm = require("../config/orm.js");
@@ -8,9 +8,56 @@ var orm = require("../config/orm.js");
 // that will call the ORM functions 
 // using burger specific input for the ORM.
 
-var burger = {
 
-}
+
+
+var burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    })
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// ***********************************************
+// ***********************************************
+
+// var cat = {
+//   all: function(cb) {
+//     orm.all("cats", function(res) {
+//       cb(res);
+//     });
+//   },
+//   // The variables cols and vals are arrays.
+//   create: function(cols, vals, cb) {
+//     orm.create("cats", cols, vals, function(res) {
+//       cb(res);
+//     });
+//   },
+//   update: function(objColVals, condition, cb) {
+//     orm.update("cats", objColVals, condition, function(res) {
+//       cb(res);
+//     });
+//   },
+//   delete: function(condition, cb) {
+//     orm.delete("cats", condition, function(res) {
+//       cb(res);
+//     });
+//   }
+// };
+
+// ***********************************************
+// ***********************************************
 
 
 // Export at the end of the burger.js file.
